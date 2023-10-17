7ba8ef13a18b53fc62996c8f7ef071d524f91639
Thomaz <thomaz2pinheiro@gmail.com>
2023-10-17 15:18:16 -0300

    FUNCIONANDO

--- src/models/mailer.js
+++ src/models/mailer.js
@@ -13,11 +13,20 @@ var transport = nodemailer.createTransport({
     }
 });
 
+// transport.use('compile', hbs({
+//     viewEngine: 'handlebars',
+//     viewPath: path.resolve('./src/resources/mail/'),
+//     // viewPath: path.resolve(__dirname, '..', 'resources', 'mail'),
+//     extName: '.html'
+// }))
+
 transport.use('compile', hbs({
-    viewEngine: 'handlebars',
-    // viewPath: path.resolve('./src/resources/mail/'),
-    viewPath: path.resolve(__dirname, '..', 'resources', 'mail'),
-    extName: '.html'
-}))
+  viewEngine: {
+    defaultLayout: undefined,
+    partialsDir: path.resolve('./src/resources/mail/')
+  },
+  viewPath: path.resolve('./src/resources/mail/'),
+  extName: '.html',
+}));
 
 module.exports = transport;
